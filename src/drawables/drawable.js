4e2059ffb425823be3205823fd9d5b36a1516ef6
David Yahalomi <davidyaha@gmail.com>
2016-04-23 16:55:20 +0300

    Special ability

--- src/drawables/drawable.js
+++ src/drawables/drawable.js
@@ -11,13 +11,14 @@ function Drawable() {
     this.y = y;
     this.width = width;
     this.height = height;
-  }
+  };
 
   this.speed = 0;
   this.canvasWidth = 0;
   this.canvasHeight = 0;
   this.collidableWith = "";
   this.isColliding = false;
+  this.collidingWith = null;
   this.type = "";
 
   // Define abstract function to be implemented in child objects
